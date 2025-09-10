8bef3b2c512af69d28f7af6e9af49cab3f2315ce
perrottij831 <perrottij@rider.edu>
2025-09-10 16:47:34 -0400

    Update Welcome1.java

--- Welcome1.java
+++ Welcome1.java
@@ -7,3 +7,10 @@ public class Welcome1 {
         System.out.println("Welcome to Java Programming!");
     }
 }
+
+public class Welcome2 {
+    public static void main(String[] args) {
+        System.out.print("Welcome to");
+        System.out.println("Java Programming!");
+    }
+}
